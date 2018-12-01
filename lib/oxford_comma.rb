a030a06699d4041893fc6e235b44d38833899323
Don Ward <10is0420@gmail.com>
2018-12-01 22:14:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,36 @@
 def oxford_comma(array)
+  
+  array_length = array.count
+  
+  if array_length == 1
+    
+    array.join()
+    
+  elsif array_length == 2
+  
+    array.insert( 1, " and ")
+    
+    array.join()
+    
+  elsif array_length == 3
+    
+    string = array.join(", ")
+    
+    string.insert(13, " and")
+    
+  elsif array_length == 5
+  
+    string = array.join(", ")
+    
+    string.insert(32, " and")
+    
+  elsif array_length == 7
+  
+    string = array.join(", ")
+    
+    string.insert(56, " and")
+  else
+    
+  end
 
 end
